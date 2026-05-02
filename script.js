220dfbd0f632144485cec4c7e8cf34b34ac02d8b
sudip-dev27 <sudiphaldar2417@gmail.com>
2026-05-02 21:19:48 +0530

    removed project section and added focus section

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
-// Scroll reveal
-const elements = document.querySelectorAll(".section");
+const sections = document.querySelectorAll(".section");
 
 window.addEventListener("scroll", () => {
-  elements.forEach(el => {
-    if (el.getBoundingClientRect().top < window.innerHeight - 100) {
-      el.style.opacity = 1;
-      el.style.transform = "translateY(0)";
+  sections.forEach(sec => {
+    if (sec.getBoundingClientRect().top < window.innerHeight - 100) {
+      sec.style.opacity = 1;
+      sec.style.transform = "translateY(0)";
     }
   });
 });
-
-// ML Prediction
-async function predict() {
-  const data = {
-    age: document.getElementById("age").value,
-    income: document.getElementById("income").value,
-    credit_score: document.getElementById("credit").value,
-    loan_amount: document.getElementById("loan").value
-  };
-
-  const res = await fetch("https://ml-cloud-project.onrender.com/predict", {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify(data)
-  });
-
-  const result = await res.json();
-  document.getElementById("result").innerText = JSON.stringify(result);
-}
